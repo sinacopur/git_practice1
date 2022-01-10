2cd5ad1caf04be8c5373bce6bfbf6e1246e25bd6
sinacopur <sinacopur@gmail.com>
2022-01-10 14:45:13 -0500

    work 3 is done and about to be pushed to Github after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,12 @@
 public class HelloWorld {
 
-    //Doing some work here (you can type anyhting you want)
+    //Doing some work here (you can type anything you want)
 
     //Doing awesome work 2 !
+
+    //Doing awesome work 3 !
+    //Awesome work 3 is done and ready to be committed
+    //ad ready to be pushed to remote repository
 
 
 
